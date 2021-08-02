4164ecad839b20969f06fb76fc42e6cb0b2e1981
Edgar alfaro <alfa-_01@outlook.es>
2021-08-01 22:45:43 -0500

    feat: aadding colors

--- src/constans/Colors.js
+++ src/constans/Colors.js
@@ -1,6 +1,8 @@
 export const Colors = {
-    bgprimary: '#5ca2d4',
-    bgsecundary:'#283546',
-    letter: '#79818a',
-    white:'#fbfbfb'
+    bgprimary: '#5ca0d3',
+    bgsecundary:'#2c3848',
+    letter: '#a6acb2',
+    white:'#ffffff',
+    bgButton:'#6b737f',
+    bg4k:'#6a727d'
 }
