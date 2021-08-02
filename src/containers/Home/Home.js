a68bd8d6fdf285e1d82db84d931cc46d322d6803
Edgar alfaro <alfa-_01@outlook.es>
2021-08-01 22:46:05 -0500

    feat: adding primary view

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,34 +1,79 @@
-import React from 'react'
-import {Text,StyleSheet,View,TouchableOpacity, Dimensions} from 'react-native'
+import React,{useState,useEffect} from 'react'
+import {Text,StyleSheet,View,TouchableOpacity, Dimensions,FlatList, ScrollView,Image, StatusBar} from 'react-native'
 import Card from '../../components/Card'
 import { Colors } from '../../constans/Colors'
+import ContainerCard from '../../components/ContainerCard'
+import Search from '../../components/Search';
+import BackFavorite from '../../components/BackFavorite' 
+import Detail from '../../components/Detail'
+import { uriApi,apiKey } from '../../constans/api'
 const Home = () =>{
-
-    const item = {
-        title:'Aquaman',
-        image:'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2019/03/Aquaman%20cartel%20b_2.jpg?itok=Ai7Sb2c9'
+    const [showView, setShowView] = useState(false)
+    const [itemTopRated, setItemTopRated] = useState([])
+    const [itemRecommended, setItemRecommended] = useState([])
+    const [itemDetail, setItemDetail] = useState({})
+    const getTopRated = async ()=>{
+        const query = await fetch(`${uriApi}top_rated?api_key=${apiKey}&language=en-US&page=2`)
+        .then((data) => data.json())
+        .then((dataJSON) => {
+          console.log(dataJSON)
+          setItemTopRated(dataJSON.results)
+        })
     }
+
+    const getRecommended= async ()=>{
+        const query = await fetch(`${uriApi}popular?api_key=${apiKey}&language=en-US&page=1`)
+        .then((data) => data.json())
+        .then((dataJSON) => {
+          console.log('recommen',dataJSON)
+          setItemRecommended(dataJSON.results)
+        })
+    }
+
+    const sendRequest = async (item)=>{
+        console.log('item',item)
+        const query = await fetch(`${uriApi}${item.id}?api_key=${apiKey}&language=en-US`)
+        .then((data) => data.json())
+        .then((dataJSON) => {
+          setItemDetail(dataJSON)
+          setShowView(true)
+          
+        })
+    }
+
+    const back= ()=>{
+        setShowView(false)
+    }
+
+    useEffect(() => {
+        getRecommended(); 
+        getTopRated();
+    }, [])
+
     return(
-        <View style={styles.container}>  
-            <View style={styles.containerHeader}>
-                <Text style={styles.title}>Hello, what do you want to watch ?</Text>
-            </View>
-            <View style={styles.containerCard}>
-                <View style={styles.containertittletext}>
-                    <Text style={styles.titletext}>RECOMMENDED FOR YOU</Text>
-                    <TouchableOpacity style={styles.containertitletextT}>
-                        <Text style={styles.titletextT}>See all</Text>
-                    </TouchableOpacity>
+        
+        !showView ? (
+            <View style={styles.container}>  
+                 <View style={styles.containerHeader}>
+                    <Text style={styles.title}>Hello, what do you want to watch ?</Text>
+                    <Search></Search>
                 </View>
-                <Card item={item}/>
-            </View>
-        </View>
-    )
+    
+                <ScrollView style={styles.containerCard}>
+                    <ContainerCard title='RECOMMENDED FOR YOU' sendRequest={sendRequest} items={itemRecommended}/>
+                    <ContainerCard title='TOP RATED' sendRequest={sendRequest} items={itemTopRated}/>
+                </ScrollView>
+            </View>)
+            :  (<View style={styles.containerDetail}>
+                    <Detail itemDetail={itemDetail} back={back}/>
+                </View> )
+    ) 
+        
 }
 
 const styles = StyleSheet.create({
     title:{
-        fontSize:25,
+        fontSize:22,
         fontWeight:'bold',
         color: Colors.white,
         paddingHorizontal: Dimensions.get('window').width * 0.1,
@@ -40,16 +85,16 @@ const styles = StyleSheet.create({
         flexDirection:'column'
     },
     containerHeader:{
-        flex:0.25,
+        flex:0.35,
         justifyContent:'center'
     },
     containerCard:{
-        padding:20,
-        flex:0.75,
+        flex:0.20,
         backgroundColor:Colors.bgsecundary,
         borderTopEndRadius:30,
         borderTopLeftRadius:30,
-        marginTop:10
+        paddingTop:20,
+        paddingLeft: Dimensions.get('window').width * 0.07
     },
     titletext:{
         color:Colors.white,
@@ -58,7 +103,8 @@ const styles = StyleSheet.create({
     },
     containertittletext:{
         flexDirection:'row',
-        marginVertical:20
+        marginHorizontal:40,
+        marginTop:35
     },
     titletextT:{
         color:Colors.white,
@@ -67,6 +113,26 @@ const styles = StyleSheet.create({
     containertitletextT:{
         flex:0.4,
         justifyContent:'flex-end'
+    },
+    containerTopRated:{
+        flex:0.75,
+        backgroundColor:Colors.bgsecundary,
+        paddingLeft: Dimensions.get('window').width * 0.07
+    },
+    containerRecommended:{
+        flex:1,
+    },
+    containerC:{
+        flex:1
+    },
+
+
+
+
+    containerDetail:{
+        backgroundColor: Colors.bgsecundary,
+        flex:1,
+        flexDirection:'column'
     }
 })
 
