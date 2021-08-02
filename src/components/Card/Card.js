cf37dc2ec273a0c077d22e55af886cad3bf7d30f
Edgar alfaro <alfa-_01@outlook.es>
2021-08-01 22:39:29 -0500

    feat: adding component card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,33 +1,76 @@
 import React from 'react'
-import {View,StyleSheet, Dimensions,Image,Text} from 'react-native'
+import {View,StyleSheet, Dimensions,Image,Text,TouchableOpacity} from 'react-native'
 import { Colors } from '../../constans/Colors'
-const Card = ({item})=>{
-    console.log('dsadsadsa',item.image)
+import Stars from 'react-native-stars';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+const Card = ({item,sendRequest})=>{
+    
     return (
-        <View style={styles.container}>
-
+        <TouchableOpacity onPress={()=>sendRequest(item)} style={styles.container}>
             <Image
-                resizeMode='cover'
+                resizeMode='stretch'
                 style={styles.image}
-                source={{uri: `${item.image}`}}
+                source={{uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`}}
             />
-            <Text style={styles.text}>{item.title}</Text>
-        </View>
+            <View style={styles.containerText}>
+                <Text style={styles.text} numberOfLines={2}>{item.original_title}</Text>
+            </View>
+            
+            <View style={styles.containerStars}>
+              <Stars
+                    default={item.vote_average/2}
+                    count={5}
+                    half={true}
+                    starSize={50}
+                    fullStar={<Icon name={'star'} style={[styles.myStarStyle]}/>}
+                    emptyStar={<Icon name={'star-outline'} style={[styles.myStarStyle, styles.myEmptyStarStyle]}/>}
+                    halfStar={<Icon name={'star-half'} style={[styles.myStarStyle]}/>}
+                />
+            </View>
+        </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create ({
     container: {
-        borderRadius: 30
+        borderRadius: 60,
+        width:Dimensions.get('window').width *0.37
+        //backgroundColor:'blue'
     },
     image:{
-        borderRadius:30,
-        width: Dimensions.get('window').width *0.35,
-        height:Dimensions.get('window').height *0.25
+        borderRadius:60,
+        width: Dimensions.get('window').width *0.3,
+        height:Dimensions.get('window').height *0.2
     },
     text:{
-       fontSize: 16,
-       color:Colors.white
-    }
+       fontSize: 12,
+       color:Colors.white,
+       //backgroundColor:'red',
+       
+       alignContent:'center'
+    },
+    stair:{
+        backgroundColor:Colors.bgsecundary
+    },
+    myStarStyle: {
+        color: 'yellow',
+        backgroundColor: 'transparent',
+        textShadowColor: 'black',
+        textShadowOffset: {width: 1, height: 1},
+        textShadowRadius: 2,
+      },
+      myEmptyStarStyle: {
+        color: 'white',
+      },
+      containerStars:{
+          alignItems:'flex-start'
+      },
+      containerText:{
+        width:Dimensions.get('window').width *0.3,
+        height:Dimensions.get('window').height *0.07,
+        //backgroundColor:'red',
+        justifyContent:'center'
+      }
+
 })
 export default Card;
